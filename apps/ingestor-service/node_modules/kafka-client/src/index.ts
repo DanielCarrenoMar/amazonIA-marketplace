5e2964e13f66763b2c95f50ed99ecf32f732ae3c
earmasag <eduardocaballeroarmas10@gmail.com>
2026-05-25 19:31:23 -0400

    Remove node_modules from git tracking

--- apps/ingestor-service/node_modules/kafka-client/src/index.ts
+++
@@ -1,6 +0,0 @@
-export { createKafkaClient } from './kafka.config';
-export { KafkaProducerService } from './producer';
-export { KafkaConsumerService } from './consumer';
-export type { ConsumedMessage } from './consumer';
-export { KAFKA_TOPICS } from './topics';
-export type { KafkaTopic } from './topics';
--- apps/telemetry-worker/node_modules/event-types/src/user-account/update-user-account.dto.ts
+++
@@ -1,6 +0,0 @@
-import { OmitType, PartialType } from '@nestjs/mapped-types';
-import { CreateUserAccountDto } from './create-user-account.dto';
-
-export class UpdateUserAccountDto extends PartialType(
-  OmitType(CreateUserAccountDto, ['password'] as const),
-) {}
